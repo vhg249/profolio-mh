751768c83dcd74eab3a8043ee06247a3dbf6b709
vhg249 <vhuongptit@gmail.com>
2023-10-14 15:02:34 +0700

    update

--- action.js
+++ action.js
@@ -8,29 +8,29 @@ $(document).ready(function () {
         $('#frame-1-before').hide();
         $("#frame-1-after").show();
     });
-    // $('#frame-1-before').mouseleave (function(){
-    //     $('#frame-1-before').show();
-    //     $("#frame-1-after").hide();
-    // });
+    $('#frame-1-before').mouseleave (function(){
+        $('#frame-1-before').show();
+        $("#frame-1-after").hide();
+    });
 
      // frame image 2
     $('#frame-2-before').mouseover (function(){
         $('#frame-2-before').hide();
         $("#frame-2-after").show();
     });
-    // $('#frame-2-before').mouseleave (function(){
-    //     $('#frame-2-before').show();
-    //     $("#frame-2-after").hide();
-    // });
+    $('#frame-2-before').mouseleave (function(){
+        $('#frame-2-before').show();
+        $("#frame-2-after").hide();
+    });
 
      // frame image 3
     $('#frame-3-before').mouseover (function(){
         $('#frame-3-before').hide();
         $("#frame-3-after").show();
     });
-    // $('#frame-3-before').mouseleave (function(){
-    //     $('#frame-3-before').show();
-    //     $("#frame-3-after").hide();
-    // });
+    $('#frame-3-before').mouseleave (function(){
+        $('#frame-3-before').show();
+        $("#frame-3-after").hide();
+    });
     
 });
